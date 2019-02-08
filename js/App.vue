206604d01b30f20ac08fad22e50c284da14dbab7
Hisham Haniffa <hishamhaniffa4@gmail.com>
2019-02-09 00:43:30 +0400

    Add cursor drag/drop color support

--- js/App.vue
+++ js/App.vue
@@ -3,10 +3,10 @@
         <h1 class="display-4">PIXEL DRAW</h1>
         <div class="row">
             <div class="color-picker">
-                <Pixel v-for="color in colorPalette" key="color" :class="[color,{active: activeColor==color}]" @click.native="selectColor(color)"></Pixel>
+                <Pixel v-for="color in colorPalette" :key="color" :class="[color,{active: activeColor==color}]" @click.native="selectColor(color)"></Pixel>
             </div>
             <div class="canvas">
-                <Pixel v-for="n in 20*20" :key="n" :color="colorArr[n]" @click.native="fillPixel(n)"></pixel>
+                <Pixel v-for="n in 20*20" :key="n" :color="colorArr[n]" @click.native="fillPixel(n)" @mousemove.native="onMove($event, n)" @mouseup.native="stopDrag" @mousedown.native="startDrag"></pixel>
             </div>
         </div>
         <div class="row">
@@ -27,12 +27,24 @@
             return {
                 activeColor: 'green',
                 colorArr: Array(401).fill('white'),
-                colorPalette: ['green', 'blue', 'red', 'yelloworange', 'black', 'orange', 'yellow', 'white']
+                colorPalette: ['green', 'blue', 'red', 'yelloworange', 'black', 'orange', 'yellow', 'white'],
+                dragging: false
             }
         },
         methods: {
             selectColor(clr) {
             this.activeColor = clr
+            },
+            startDrag() {
+                this.dragging = true;
+            },
+            stopDrag() {
+                this.dragging = false;
+            },
+            onMove(evt, n) {
+                if(this.dragging) {
+                    this.colorArr.splice(n,1,this.activeColor)
+                }
             },
             fillPixel(n) {
                 this.colorArr.splice(n,1,this.activeColor)
