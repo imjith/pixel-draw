9da1bbae6bb575567fd98ed0b5702934568ea0ba
Hisham Haniffa <hishamhaniffa4@gmail.com>
2019-02-08 22:28:58 +0400

    Add webpack support, add vue deps and build command to bundle the js

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+	mode: 'development',
+	entry: './js/app.js',
+	module: {
+		rules: [
+			{
+				test: /\.vue$/,
+				loader: 'vue-loader'
+			},
+			{
+				test: /\.js$/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.css$/,
+				use: [
+					'vue-style-loader',
+					'css-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new VueLoaderPlugin()
+	],
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'app.bundle.js'
+	}
+};
